4c2e4cfe6fa1e4a5224ddded8bc4650e6a46c651
yerimarbi <marbizepeda@gmail.com>
2022-10-22 17:06:21 -0500

    http-request

--- File.js
+++ File.js
@@ -1,17 +1,22 @@
 import fs from 'fs'
+import axios from 'axios'
 
-  //Imprimir el contenido de un archivo
-  const fileRoute = fs.readFileSync("./README.md", "utf-8");//Leer el contenido de un archivo
-  // console.log(fileRoute);
-
+//Imprimir el contenido de un archivo
+const fileRoute = fs.readFileSync('./README.md', "utf-8");//Leer el contenido de un archivo
+console.log(fileRoute);
 
 //Extraer los links del archivo
 const extractLinks = () => {
   const stringRoute = fileRoute.toString();
-  const regExp = /\[(.+)\]\((https?:\/\/.+)\)/gi;
+  const regExp = (/\http.*?\)/g);
   let findLinks = [stringRoute.match(regExp)];
   for (let i = 0; i < findLinks.length; i++) {
   }
   console.log(findLinks);
 }
 extractLinks();
+
+//objeto con únicamente dos propiedades validate false: href, text, file y stats: href, text, file, status, ok
+const urlOk = 'https://github.com/YeriMarbi/CDMX013-md-links';
+const urlValidate = axios.get(urlOk)
+urlValidate.then((result) => { console.log(result.statusText, result.status) });
